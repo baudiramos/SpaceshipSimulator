2c325f4ad2938419bd246182d48335a08a7f7ae2
Baudi <baudirguanche@gmail.com>
2022-04-12 18:40:32 +0100

    Feature: Se ha añadido los atributos solicitados

--- src/SpaceShip.java
+++ src/SpaceShip.java
@@ -1,2 +1,11 @@
 public class SpaceShip {
+    private String nombreNave;
+    private String matriculaNave;
+    private int aceleracion;
+    private float velocidadX;
+    private float velocidadY;
+    private float coordenadaX;
+    private float coordenadaY;
+    private float direccionX;
+    private float direccionY;
 }
