8e48e16ec870023a5f9f7537c096d0d6c58cbc30
Baudi <baudirguanche@gmail.com>
2022-04-12 18:45:23 +0100

    Feature: Se ha iniciado el constructor

--- src/SpaceShip.java
+++ src/SpaceShip.java
@@ -8,4 +8,19 @@ public class SpaceShip {
     private float coordenadaY;
     private float direccionX;
     private float direccionY;
+
+    // Iniciamos constructor
+
+
+    public SpaceShip(String nombreNave, String matriculaNave, int aceleracion, float velocidadX, float coordenadaX, float direccionX) {
+        this.nombreNave = nombreNave;
+        this.matriculaNave = matriculaNave;
+        this.aceleracion = aceleracion;
+        this.velocidadX = velocidadX;
+        this.coordenadaX = coordenadaX;
+        this.direccionX = direccionX;
+    }
+
+
+
 }
