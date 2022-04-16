478cf96604cb7d573658e8c4d14744ad6905e2be
Baudi <baudirguanche@gmail.com>
2022-04-16 19:01:29 +0100

    Feature: Se ha creado el método unload con su parámetro

--- src/Cargo.java
+++ src/Cargo.java
@@ -1,5 +1,6 @@
 public class Cargo  extends  SpaceShip{
     private  int cargaMaxima;
+    private  int capacidadBodega;
 
     public int  load(int cargaMaxima) {
         cargaMaxima = cargaMaxima;
@@ -7,6 +8,14 @@ public class Cargo  extends  SpaceShip{
             System.out.println("Hay espacio para intrudicir carga en la bodea");
         } else {
             System.out.println("No hay espacio para introducir más carga en la bodega");
+        }
+    }
+
+    public int unload(int capacidadBodega) {
+        if (capacidadBodega >= 1) {
+            System.out.println("Hay algun container en la bodega, se va a eliminar");
+        } else {
+            System.out.println("No se puede descargar ningun container de  la bodega porque no hay ninguno disponible");
         }
     }
 
