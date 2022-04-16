f2462c1ec47de3ab87fe39f68cedc361cca13b99
Baudi <baudirguanche@gmail.com>
2022-04-16 18:58:03 +0100

    Feature: Se ha creado el método load

--- src/Cargo.java
+++ src/Cargo.java
@@ -1,4 +1,17 @@
 public class Cargo  extends  SpaceShip{
     private  int cargaMaxima;
 
+    public int  load(int cargaMaxima) {
+        cargaMaxima = cargaMaxima;
+        if (cargaMaxima >= 1) {
+            System.out.println("Hay espacio para intrudicir carga en la bodea");
+        } else {
+            System.out.println("No hay espacio para introducir más carga en la bodega");
+        }
+    }
+
+
+    public Cargo(String nombreNave, String matriculaNave, int aceleracion, float velocidadX, float coordenadaX, float direccionX) {
+        super(nombreNave, matriculaNave, aceleracion, velocidadX, coordenadaX, direccionX);
+    }
 }
