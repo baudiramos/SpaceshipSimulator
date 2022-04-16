bedb367fba3b9c9b809ea5407d9033678029864f
Baudi <baudirguanche@gmail.com>
2022-04-16 19:03:33 +0100

    Refactor: Se ha corregido algunos errores en los métodos load y unload

--- src/Cargo.java
+++ src/Cargo.java
@@ -3,9 +3,9 @@ public class Cargo  extends  SpaceShip{
     private  int capacidadBodega;
 
     public int  load(int cargaMaxima) {
-        cargaMaxima = cargaMaxima;
         if (cargaMaxima >= 1) {
             System.out.println("Hay espacio para intrudicir carga en la bodea");
+            capacidadBodega = cargaMaxima;
         } else {
             System.out.println("No hay espacio para introducir más carga en la bodega");
         }
@@ -14,10 +14,15 @@ public class Cargo  extends  SpaceShip{
     public int unload(int capacidadBodega) {
         if (capacidadBodega >= 1) {
             System.out.println("Hay algun container en la bodega, se va a eliminar");
+            capacidadBodega = 0;
         } else {
             System.out.println("No se puede descargar ningun container de  la bodega porque no hay ninguno disponible");
         }
     }
+
+
+
+
 
 
     public Cargo(String nombreNave, String matriculaNave, int aceleracion, float velocidadX, float coordenadaX, float direccionX) {
