f9cbb2c9971a353844a4ae4147ff80fa0e32ddc2
Baudi <baudirguanche@gmail.com>
2022-04-20 17:00:34 +0100

    Feature: Se ha cambiado los metodos

--- src/Cargo.java
+++ src/Cargo.java
@@ -2,27 +2,28 @@ public class Cargo  extends  SpaceShip{
     private  int cargaMaxima;
     private  int capacidadBodega;
 
-    public int  load(int cargaMaxima) {
+    public void  load(int cargaMaxima) {
+
         if (cargaMaxima >= 1) {
+
             System.out.println("Hay espacio para intrudicir carga en la bodea");
             capacidadBodega = cargaMaxima;
         } else {
             System.out.println("No hay espacio para introducir más carga en la bodega");
         }
+
     }
 
-    public int unload(int capacidadBodega) {
+    public void unload(int capacidadBodega) {
+
         if (capacidadBodega >= 1) {
             System.out.println("Hay algun container en la bodega, se va a eliminar");
             capacidadBodega = 0;
         } else {
             System.out.println("No se puede descargar ningun container de  la bodega porque no hay ninguno disponible");
         }
+
     }
-
-
-
-
 
 
     public Cargo(String nombreNave, String matriculaNave, int aceleracion, float velocidadX, float coordenadaX, float direccionX) {
