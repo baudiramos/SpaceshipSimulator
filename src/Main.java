601db7c8965db4b5dd5d8bc066ddc8655f598469
Baudi <baudirguanche@gmail.com>
2022-04-21 13:42:22 +0100

    Feature: Se ha añadido el menú

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,19 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String menu;
 
+        while (condicion) {
+
+            System.out.println("1. Crear nave");
+
+            System.out.println("2. Mostrar asciiArt de la nave");
+
+            System.out.println("3. Acelerar nave");
+
+            System.out.println("4. Frenar nave");
+
+            System.out.println("5. Parar nave");
+
+        }
+
     }
 }
